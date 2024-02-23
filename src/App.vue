d8fbbc17e12556dbef205c09d27fec5b72f90807
이준용 <iiiuooyoo@gmail.com>
2024-02-23 14:47:09 +0900

    feat : 날짜 선택기 추가

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <template>
-    <div>Hello World!</div>
+    <div>
+        <DatePicker @onChange="handleChange" />
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from "vue";
+
+import { DatePicker } from "./components";
+
+type DateConfig = {
+    start: string;
+    end: string;
+};
+
+const $date = reactive<DateConfig>({
+    start: "",
+    end: "",
+});
+
+function handleChange({ start, end }: DateConfig) {
+    $date.start = start;
+    $date.end = end;
+}
+</script>
 
 <style></style>
---
+++ src/components/DatePicker.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div>
+            <h3>시작 일</h3>
+            <input
+                type="date"
+                v-model="config.start"
+                @change="emits('onChange', config)"
+            />
+        </div>
+        <div>
+            <h3>종료 일</h3>
+            <input
+                type="date"
+                v-model="config.end"
+                @change="emits('onChange', config)"
+            />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { reactive } from "vue";
+
+const emits = defineEmits(["onChange"]);
+
+const config = reactive({
+    start: "",
+    end: "",
+});
+</script>
+
+<style></style>
